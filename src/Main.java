39159d8a979b085bded9df8d344e547ad3cb8982
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-18 15:15:54 +0100

    Solved

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,28 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         // Hitta och skriv ut de första 50 primtalen.
 
+        int[] primes = new int[50];
+        int count = 0;
+        int number = 1;
 
+        while (count < primes.length) {
+            number++;
+            boolean isPrime = true;
+            for (int i=0; i < count; i++) {
+                if (number % primes[i] == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                primes[count] = number;
+                count++;
+            }
+        }
+
+        System.out.println(Arrays.toString(primes));
     }
 }
